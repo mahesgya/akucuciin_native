6312d858cc9bab1c8b250fb7f2804a82a335c14e
unknown <anargya115@gmail.com>
2025-05-04 20:38:38 +0700

    feat: make UI HeaderHome

---
+++ app/ui/home/header.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { colors } from "@/app/constants/colors";
+
+type HeaderHomeProps = {
+    title : string;
+    totalAmount?: number;
+}
+
+const HeaderHome: React.FC<HeaderHomeProps> = ( {title, totalAmount} ) => {
+    return(
+        <View>
+            <View>
+                <Text> {title} </Text>
+            </View>
+
+            {totalAmount && (
+            <View>
+                <Text>Rp{totalAmount}</Text>
+            </View>
+            )}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#ffffff",
+        paddingVertical: 12,
+        paddingHorizontal: 15,
+    },
+    topContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingVertical: 5,
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: colors.primary,
+    },
+    amountContainer: {
+        marginTop: 5,
+    },
+    amountText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: colors.selesai,
+    },
+})
+
+export default HeaderHome;
