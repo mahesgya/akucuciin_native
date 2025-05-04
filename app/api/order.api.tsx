c98a38bbc2206bb42f3766bd4bc31cd3d6f6b17d
unknown <anargya115@gmail.com>
2025-05-04 10:10:56 +0700

    feat: make getOrderById and updateStatusOrder fetch

--- app/api/order.api.tsx
+++ app/api/order.api.tsx
@@ -15,7 +15,28 @@ const OrderApi = {
             throw error; 
         }
     },
- 
+    getOrderById : async (idOrder:string, accessToken:string) => {
+        try {
+            const response = await axios.get(`${API_URL}/api/laundry_partner/app/order/${idOrder}`, {
+                headers : {Authorization: `Bearer ${accessToken}`}
+            })
+
+            return response.data.data
+        } catch (error) {
+            throw error;
+        }
+    },
+    updateStatusOrder : async (formData:any, idOrder:string, accessToken:string) => {
+        try {
+            const response = await axios.put(`${API_URL}/api/laundry_partner/app/order/${idOrder}`, formData, {
+                headers : {Authorization: `Bearer ${accessToken}`}
+            })
+
+            return response.data.data
+        } catch (error) {
+            throw error
+        }
+    }
 }
 
 export default OrderApi
