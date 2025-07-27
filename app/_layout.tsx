b337976bcbcfeef64b024a8d16682417cf90e57a
mahesgya <anargya115@gmail.com>
2025-07-27 21:14:18 +0800

    feat: add alertprovider at layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Slot } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
+import { AlertProvider } from "./hooks/alert";
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <Slot />
-    </SafeAreaProvider>
+    <AlertProvider>
+      <SafeAreaProvider>
+        <Slot />
+      </SafeAreaProvider>
+    </AlertProvider>
   );
 }
